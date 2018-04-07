47f06b492c9ce2477e57880d8f569fc43b7ebc8f
unknown <1037196370@qq.com>
2018-04-07 17:03:04 +0800

    改造键盘的jni到jna

--- src/main/java/motionSimulation/KeyboardMotion.java
+++ src/main/java/motionSimulation/KeyboardMotion.java
@@ -1,13 +1,21 @@
 package motionSimulation;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
 /**
  * 通过jni控制键盘事件
  */
 public class KeyboardMotion {
-    public static native void keyDown(int key);
-    public static native void keyUp(int key);
-
-    static {
-        System.loadLibrary("lib/KeyboardMotion");
+    public interface KeyBoard extends Library {
+        KeyBoard INSTANCE = ( KeyBoard) Native.loadLibrary("lib/KeyBoard",  KeyBoard.class);
+        public void keyDown(int a);
+        public void keyUp(int a);
+    }
+    public static void keyDown(int a){
+        KeyBoard.INSTANCE.keyDown(a);
+    }
+    public static void keyUp(int a){
+        KeyBoard.INSTANCE.keyUp(a);
     }
 }
