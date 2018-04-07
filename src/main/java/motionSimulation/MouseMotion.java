8e324e6a46573b4c7757216b7e056d26abbefd08
unknown <1037196370@qq.com>
2018-04-07 14:19:09 +0800

    将鼠标控制部分的jni改写成jna

--- src/main/java/motionSimulation/MouseMotion.java
+++ src/main/java/motionSimulation/MouseMotion.java
@@ -1,24 +1,66 @@
 package motionSimulation;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
 /**
  * 通过jni控制鼠标
  */
 public class MouseMotion {
-    public static native void up();
-    public static native void down();
-    public static native void left();
-    public static native void right();
-    public static native void moveTo(int horizontal,int vertical);
-    public static native void moveBy(int horizontal,int vertical);
-    public static native void leftDown();
-    public static native void leftUp();
-    public static native void rightDown();
-    public static native void rightUp();
-    public static native void middleDown();
-    public static native void middleUp();
-    public static native void wheelRotate(int distance);
+    public interface Mouse extends Library {
+        Mouse INSTANCE = (Mouse) Native.loadLibrary("lib/Mouse", Mouse.class);
+        public void mouseUp();
+        public void mouseDown();
+        public void mouseLeft();
+        public void mouseRight();
+        public void mouseMoveTo(int a, int b);
+        public void mouseMoveBy(int a, int b);
+        public void mouseLeftDown();
+        public void mouseLeftUp();
+        public void mouseRightDown();
+        public void mouseRightUp();
+        public void mouseMiddleDown();
+        public void mouseMiddleUp();
+        public void mouseWheelRotate(int a);
+    }
 
-    static {
-        System.loadLibrary("lib/MouseMotion");
+    public static void up(){
+        Mouse.INSTANCE.mouseUp();
+    }
+    public static void down(){
+        Mouse.INSTANCE.mouseDown();
+    }
+    public static void left(){
+        Mouse.INSTANCE.mouseLeft();
+    }
+    public static void right(){
+        Mouse.INSTANCE.mouseRight();
+    }
+    public static void moveTo(int horizontal,int vertical){
+        Mouse.INSTANCE.mouseMoveTo(horizontal,vertical);
+    }
+    public static void moveBy(int horizontal,int vertical){
+        Mouse.INSTANCE.mouseMoveBy(horizontal,vertical);
+    }
+    public static void leftDown(){
+        Mouse.INSTANCE.mouseLeftDown();
+    }
+    public static void leftUp(){
+        Mouse.INSTANCE.mouseLeftUp();
+    }
+    public static void rightDown(){
+        Mouse.INSTANCE.mouseRightDown();
+    }
+    public static void rightUp(){
+        Mouse.INSTANCE.mouseRightUp();
+    }
+    public static void middleDown(){
+        Mouse.INSTANCE.mouseMiddleDown();
+    }
+    public static void middleUp(){
+        Mouse.INSTANCE.mouseMiddleUp();
+    }
+    public static void wheelRotate(int distance){
+        Mouse.INSTANCE.mouseWheelRotate(distance);
     }
 }
